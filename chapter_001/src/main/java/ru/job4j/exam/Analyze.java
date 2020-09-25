db0c5257845893ee71552ec6ccb7184d24262e10
Maxim Rybetsky <maximrybetsky@gmail.com>
2020-09-25 11:45:06 +0300

    2. Статистику по коллекции.

--- chapter_001/src/main/java/ru/job4j/exam/Analyze.java
+++ chapter_001/src/main/java/ru/job4j/exam/Analyze.java
@@ -9,14 +9,11 @@ public class Analyze {
      * {@code current} list converts to {@code Map} object {@code currMap} via {@code getMapFromList} with {@code User}
      * id as a key and {@code User} object as a value.<br><br>
      *
-     *  The same process with {@code previous} list to {@code prevMap}.<br><br>
-     *
-     *  Then get key set of {@code prevMap} and iterate over it via foreach loop.
-     * In this loop get the value of the current key {@code i} for {@code currMap} and {@code prevMap}
-     * to {@code curr} and {@code prev} variables respectively. Not null {@code curr} means this value
-     * wasn't deleted. Comparing {@code curr} and {@code prev} in this case gives answer of changing this
-     * element of list. If it was, increase {@code changed} variable. Remove this value from {@code currMap} after
-     * this check for counting number of added values to list.<br><br>
+     *  Then iterate over {@code previous} list via foreach loop. In the loop get the value of the current {@code user}'s
+     * id for {@code currMap} to {@code curr} variable.
+     * Not null {@code curr} means this value wasn't deleted. Comparing {@code curr} and {@code user} in this case
+     * gives answer of changing this element of list. If it was, increase {@code changed} variable. Remove this value
+     * from {@code currMap} after this check for counting number of added values to list.<br><br>
      *
      *  Null {@code curr} means deleting this value from list.
      * @param previous list before modifications
@@ -25,17 +22,16 @@ public class Analyze {
      */
     public Info diff(List<User> previous, List<User> current) {
         Map<Integer, User> currMap = getMapFromList(current);
-        Map<Integer, User> prevMap = getMapFromList(previous);
         int deleted = 0;
         int changed = 0;
-        for (int i : prevMap.keySet()) {
-            User prev = prevMap.get(i);
-            User curr = currMap.get(i);
+        for (User user : previous) {
+            int userId = user.getId();
+            User curr = currMap.get(userId);
             if (curr != null) {
-                if(!curr.equals(prev)) {
+                if(!curr.equals(user)) {
                     ++changed;
                 }
-                currMap.remove(i);
+                currMap.remove(userId);
             } else {
                 ++deleted;
             }
