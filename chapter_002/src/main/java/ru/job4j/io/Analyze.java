30b8d09bbc11fe65ae324aeb59d71fbeb85c9208
MaxRybetsky <stsdewar@ya.ru>
2020-10-22 12:34:08 +0300

    2. Анализ доступности сервера.

--- chapter_002/src/main/java/ru/job4j/io/Analyze.java
+++ chapter_002/src/main/java/ru/job4j/io/Analyze.java
@@ -1,12 +1,14 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Analyze {
 
     public void unavailable(String source, String target) {
-        try (BufferedReader in = new BufferedReader(new FileReader(source));
-             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+        List<String> result = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(source))) {
             StringBuilder currString = new StringBuilder();
             in.lines().forEach(
                     e -> {
@@ -18,15 +20,24 @@ public class Analyze {
                         } else if ("200".equals(data[0]) || "300".equals(data[0])) {
                             if (currString.length() != 0) {
                                 currString.append(";").append(data[1]);
-                                out.write(currString.toString() + System.lineSeparator());
+                                result.add(currString.toString());
                                 currString.setLength(0);
                             }
                         }
                     }
             );
             if (currString.length() != 0) {
-                out.write(currString.toString());
+                result.add(currString.toString());
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        writeToFile(target, result);
+    }
+
+    private void writeToFile(String file, List<String> list) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(file))) {
+            list.forEach(e -> out.write(e + System.lineSeparator()));
         } catch (Exception e) {
             e.printStackTrace();
         }
