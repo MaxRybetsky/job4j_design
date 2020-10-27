f31d9e5bd537c16ecfd10be690169a0c8c209344
MaxRybetsky <stsdewar@ya.ru>
2020-10-27 14:58:32 +0300

    5. Валидация параметров запуска.

--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -6,8 +6,11 @@ import java.util.List;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, "properties").forEach(System.out::println);
+        if(args.length < 2) {
+            throw new IllegalArgumentException("Incorrect number of arguments.");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, args[1]).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, String ext) throws IOException {
